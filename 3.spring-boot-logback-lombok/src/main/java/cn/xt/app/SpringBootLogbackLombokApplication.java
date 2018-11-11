3c5de2b94244e36b3d6aac6907f66b7bb5683159
420298524@qq.com <420298524@qq.com>
2018-11-11 20:48:02 +0800

    版本统一

--- 3.spring-boot-logback-lombok/src/main/java/cn/xt/app/SpringBootLogbackLombokApplication.java
+++ 3.spring-boot-logback-lombok/src/main/java/cn/xt/app/SpringBootLogbackLombokApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLogbackLombokApplication {
 
-	private String set;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLogbackLombokApplication.class, args);
 	}
